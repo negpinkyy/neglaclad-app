9e21b24313008ca4f2338032f4aec704a665999d
pinky winky <mac@MACs-MacBook-Pro.local>
2024-12-28 11:07:38 +0100

    collection method

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import { useContext } from "react"
+import { ShopContext } from "../context/ShopContext"
+
+const SearchBar = () => {
+    const { search,setSearch, showSearch,setShowSearch} = useContext(ShopContext);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchBar
