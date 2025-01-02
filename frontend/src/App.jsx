23b85909a81b3296b26b46d5c52d7b3402146281
pinky winky <mac@MACs-MacBook-Pro.local>
2025-01-02 07:45:20 +0100

    worked on search method and product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,13 @@ import Orders from "./pages/Orders"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import SearchBar from "./components/SearchBar"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw]  md:px-[7vw] lg:px-[7vw]">
+     <ToastContainer/>
       <Navbar/>
       <SearchBar/>
       <Routes>
@@ -24,7 +27,7 @@ const App = () => {
      <Route path='/collection' element={<Collection/>} />
      <Route path='/about' element={<About/>} />
      <Route path='/contact' element={<Contact/>} />
-     <Route path='/product/:id' element={<Product/>} />
+     <Route path='/product/:productId' element={<Product/>} />
      <Route path='/cart' element={<Cart/>}/>
      <Route path='/login' element={<Login/>}/>
      <Route path='/place-order' element={<PlaceOrder/>}/>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,99 @@
 // import React from 'react'
 
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom"
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
+import RelatedProduct from "../components/RelatedProduct";
+
 const Product = () => {
-  return (
-    <div>
-      
+
+const {productId} = useParams();
+const {products, currency, addToCart} =  useContext(ShopContext);
+const [productData, setProductData] = useState(false);
+const [image, setImage] = useState('')
+const [size, setSize] = useState('')
+
+const fetchProductData = async () =>{
+
+products.map((item)=>{
+  if(item._id === productId){
+    setProductData(item);
+    setImage(item.image[0])
+    // console.log(item)
+    return null;
+  }
+})
+}
+
+ useEffect(()=>{
+fetchProductData();
+},[productId])
+
+
+  return productData ?(
+    <div className="transition-opacity ease-in border border-t-2 dueation-500 opacity-1">
+      {/* product data */}
+      <div className="flex flex-col gap-12 sm:gap-12 sm:flex-row">
+        {/* products images */}
+        <div className="flex flex-col-reverse flex-1 gap-3 sm:flex-row">
+          <div className="flex flex-col justify-between w-full overflow-x-auto sm:justify-normal sm:w-[17.8%]">
+            {
+              productData.image.map((item,index)=>(
+                <img src={item}  onClick={()=>setImage(item)} alt="" className="w-[24%] sm:w-full sm:mb-5 flex-shrink-4 cursor-pointer" key={index} />
+              ))
+            }
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="" className="w-full h-auto" />
+          </div>
+        </div>
+        {/* product info */}
+        <div className="flex-1">
+          <h1 className="mt-2 text-2xl font-medium">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="min-w-3.5" />
+            <img src={assets.star_icon} alt="" className="min-w-3.5" />
+            <img src={assets.star_icon} alt="" className="min-w-3.5" />
+            <img src={assets.star_icon} alt="" className="min-w-3.5" />
+            <img src={assets.star_dull_icon} alt="" className="min-w-3.5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
+          <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+          <div className="flex flex-col gap-4 my-6">
+            <p className="">Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index)=>(
+                <button onClick={()=>setSize(item)}  key={index} className={`border py-2 px-4 bg-gray-100 ${item=== size ? 'border border-orange-500' : ''} `}>{item}</button>
+                ))}
+            </div>
+          </div>
+          <button onClick={()=>addToCart(productData._id, size)} className="px-8 py-3 text-sm text-white bg-black active:bg-gray-700">ADD TO CART</button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="flex flex-col gap-1 mt-5 text-sm text-gray-500">
+            <p className="">100% original Product</p>
+            <p className="">Cash on delivery is available on this product</p>
+            <p className="">Easy return and exchange policy within 7 days</p>
+          </div>
+        </div>
+      </div>
+      {/* description and review section */}
+      <div className="mt-20">
+        <div className="flex ">
+          <p className="px-5 py-3 text-sm border">Description </p>
+          <p className="px-5 py-3 text-sm border">Reviews (122) </p>
+        </div>
+        <div className="flex flex-col gap-4 px-6 py-6 text-sm text-gray-500 border ">
+          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam minus neque nesciunt, eius blanditiis vel dolorum quaerat nobis deleniti.</p>
+          <p className="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, tempore!</p>
+        </div>
+      </div>
+      {/* related products section */}
+      <RelatedProduct category={productData.category} subCategory={productData.subCategory}/>
     </div>
-  )
+  ):<div className="opacity-0"></div>
+
 }
 
 export default Product
